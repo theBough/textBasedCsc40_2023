64575a4d58dfed133c7b93665740d7304d44e57a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-03-21 15:14:44 -0500

    Create rooms.js

---
+++ rooms.js
@@ -0,0 +1,12 @@
+var rooms = [];
+function loadRooms(){
+  rooms = [
+    [room100, room132, lobby]
+  ]
+}
+var room100  = `Describe the room here
+I can use more lines
+with no error
+The back tick is the key above the TAB button`
+var  room132 = `you are in room 132`
+var lobby = `You are in the Lobby`
