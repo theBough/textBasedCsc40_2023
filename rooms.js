412c4f5fa62da0c8ae1c43593b1689aa2cefb82f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-03-24 15:07:12 -0500

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1,12 +1,35 @@
 var rooms = [];
 function loadRooms(){
   rooms = [
-    [room100, room132, lobby]
+    [room100, room132, lobby],
+    [room201, room200, room202]
   ]
 }
-var room100  = `Describe the room here
-I can use more lines
-with no error
-The back tick is the key above the TAB button`
-var  room132 = `you are in room 132`
-var lobby = `You are in the Lobby`
+var room100  = {
+    name:'Room 100',
+    description: `Describe the room here
+      I can use more lines
+      with no error
+      The back tick is the key above the TAB button`
+}
+  
+var  room132 = {
+    name:"Room 132",
+    description:`you are in room 132`
+}
+var lobby = {
+  name: "Lobby",
+  description:`You are in the Lobby`
+}
+var room201 = {
+  name: "Room 201",
+  description:`You are in Room 201`
+}
+var room200 = {
+  name:"Room 200",
+    description:`You are in room 200`
+}
+var room202 = {
+  name:"Room 202",
+  description:`You are in room 202`
+}
